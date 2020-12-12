d59dd695974eff9b1810814f3ca29a4128070315
chikwado17 <chikwadonworie@gmail.com>
2020-12-12 10:51:22 +0100

    making first api calls

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,15 @@
-// Global app controller
+import axios from 'axios';
+
+async function getRecipe(query) {
+  try {
+
+    const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${query}`);
+    const recipes = res.data.recipes[0];
+    console.log(recipes);
+
+  }catch(err) {
+      console.log(err);
+  }
+}
+
+getRecipe('pizza');
